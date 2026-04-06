c4a3e320ff7572a1f4f4a611a103c5d7e6be2be7
Adil Daoud <adildaoudhd@gmail.com>
2026-04-06 15:53:14 +0100

    ajouter une ligne dans hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,9 @@ public class TestDevops {
 
     public static void main(String[] args) {
         System.out.println("Hello DevOps !");
-        System.out.println("Test de la branche dev réussi ✅");
+        System.out.println("Test de la branche dev ");  
+      System.out.println("Test de la branche dev 2 ");  
+
+        
     }
 }
